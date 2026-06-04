3e778fffbfb024781f4252ec3e3db6e312b41a32
getDub <143089345+getDub@users.noreply.github.com>
2026-06-04 21:22:35 +1000

    Caesar_cipher cycles backwards and forwards through the alphabet. Starting at 'a' after it passes 'z' and going back from 'z' if it goes backward from 'a'.

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -9,11 +9,21 @@ require_relative '../lib/caeser_cipher'
 # verifying that an alery or email notification was triggered.
 
 describe '#caesar_cipher' do
+
   it 'moves each letter by the number given' do
     expect(caesar_cipher('abc', 3)).to eq('def')
   end
 
   it 'moves backward in the alphabet when given a negative number' do
     expect(caesar_cipher('def', -3)).to eq('abc')
+  end
+
+  it 'cycles back to the start of the alphabet if it passes z' do
+    expect(caesar_cipher('zzz', 1)).to eq('aaa')
+  end
+
+  it 'returns to the end of the alphabet if it goes backward past a' do
+    expect(caesar_cipher('aaa', -1)).to eq('zzz')
+    expect(caesar_cipher('mmm', -27)).to eq('lll')
   end
 end
